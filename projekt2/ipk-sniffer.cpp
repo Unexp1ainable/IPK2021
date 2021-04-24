91fd33983269c661f026f39fcbab30a0a1d6ac38
Unexp1ainable <samorepka314@gmail.com>
2021-04-24 10:10:58 +0200

    somethings workings

--- projekt2/ipk-sniffer.cpp
+++ projekt2/ipk-sniffer.cpp
@@ -4,7 +4,17 @@
 #include <getopt.h>
 #include <vector>
 #include <sstream>
+#include <memory>
+#include <net/ethernet.h>
+#include <algorithm>
 
+#define IPV6_TCP 6
+#define IPV6_UDP 17
+#define IPV6_ICMP 58
+#define IPV6_NONEXT 59
+const std::vector<int> IPV6_EXT_H_OTHERS = {0, 43, 44, 50, 51, 60, 135};
+
+using std::cerr;
 using std::cout;
 using std::endl;
 
@@ -12,14 +22,169 @@ int main(int argc, char *argv[])
 {
     auto args = parse_arguments(argc, argv);
 
-    cout << args->interface << endl;
-    cout << args->port << endl;
+    // initialise pcap
+    char errbuff[PCAP_ERRBUF_SIZE];
+    if (pcap_init(PCAP_CHAR_ENC_LOCAL, errbuff) == -1)
+    {
+        cerr << "Initialisation failed: \n";
+        cerr << errbuff << "\n";
+        return 1;
+    }
 
-    delete args;
+    // check if -i was without parameter
+    if (args->interface == "")
+    {
+        print_devices();
+        return 0;
+    }
+
+    // TODO remove
+    cout << "Interface: " << args->interface << endl;
+
+    // open interface
+    auto interface_unsafe = pcap_open_live(args->interface.c_str(), 1024, 1, 1000, errbuff);
+    if (!interface_unsafe)
+    {
+        cerr << "Failed to open the descriptor.\n";
+        return 1;
+    }
+
+    // declare custom deleter functor
+    struct del_pcap_handle
+    {
+        del_pcap_handle(){};
+        void operator()(pcap_t *handle)
+        {
+            pcap_close(handle);
+        };
+    };
+
+    // transform it to unique ptr, so it is automatically closed at the end
+    auto interface = std::unique_ptr<pcap_t, del_pcap_handle>(interface_unsafe, del_pcap_handle());
+
+    int retcode = pcap_loop(interface.get(), args->number, packet_handler, NULL);
     return 0;
 }
 
-Arguments *parse_arguments(int argc, char *argv[])
+void packet_handler(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes)
+{
+    struct ether_header *eth_header;
+    eth_header = (struct ether_header *)bytes;
+
+    if (ntohs(eth_header->ether_type) == ETHERTYPE_IP)
+    {
+        printf("IPv4 ");
+        const u_char *ip = bytes + 14;
+        int protocol = *(ip + 9);
+        int ihl = (*ip) & 0x0F;
+        cout << ihl << endl;
+
+        switch (protocol)
+        {
+        case IPPROTO_ICMP:
+            cout << "ICMP\n";
+            break;
+        case IPPROTO_UDP:
+            cout << "UDP\n";
+            break;
+        case IPPROTO_TCP:
+            cout << "TCP\n";
+            break;
+
+        default:
+            break;
+        }
+    }
+    else if (ntohs(eth_header->ether_type) == ETHERTYPE_IPV6)
+    {
+        cout << "IPv6 ";
+        const u_char *ip = bytes + 14;
+        u_char *n_header_p = const_cast<u_char *>(ip + 6);
+        int n_header = *(n_header_p);
+
+        bool end = false;
+
+        do
+        {
+            switch (n_header)
+            {
+            case IPV6_TCP:
+                cout << "TCP\n";
+                end = true;
+                break;
+            case IPV6_UDP:
+                cout << "UDP\n";
+                end = true;
+                break;
+            case IPV6_ICMP:
+                cout << "ICMP\n";
+                end = true;
+                break;
+            case IPV6_NONEXT:
+                cout << "Unknown\n";
+                end = true;
+                break;
+
+            default:
+                if (is_other_ext(n_header))
+                {
+                    n_header_p = (n_header_p + *(n_header_p + 1));
+                    n_header = *n_header_p;
+                }
+                else {
+                    end = true;
+                }
+                
+                break;
+            }
+
+        } while (!end);
+    }
+    else if (ntohs(eth_header->ether_type) == ETHERTYPE_ARP)
+    {
+        printf("ARP\n");
+    }
+}
+
+
+
+bool is_other_ext(int header) {
+    auto begin_iterator = IPV6_EXT_H_OTHERS.begin();
+    auto end_iterator = IPV6_EXT_H_OTHERS.end();
+    if (std::find(begin_iterator, end_iterator, header) != end_iterator){
+        return true;
+    }
+    return false;
+}
+
+
+
+void print_devices()
+{
+    pcap_if_t *devices;
+    char errbuff[PCAP_ERRBUF_SIZE];
+    if (pcap_findalldevs(&devices, errbuff) == -1)
+    {
+        cerr << "Failed to find devices.\n ";
+    }
+
+    cout << "Devices avaliable:\n";
+    // print all devices
+    for (pcap_if_t *temp = devices; temp; temp = temp->next)
+    {
+        cout << "\t" << temp->name << "\n";
+    }
+    pcap_freealldevs(devices);
+}
+
+/**
+ * @brief Parse arguments
+ * 
+ * @param argc Number of arguments
+ * @param argv Pointer to argument array
+ * @return std::unique_ptr<Arguments> Instance of Arguments class filled with arguments.
+ */
+std::unique_ptr<Arguments> parse_arguments(int argc, char *argv[])
 {
     enum option_identifiers
     {
@@ -127,7 +292,6 @@ Arguments *parse_arguments(int argc, char *argv[])
         case NUMBER:
         {
             std::stringstream ss(optarg);
-
             if (!(ss >> number))
                 throw std::bad_cast{};
             break;
@@ -151,5 +315,5 @@ Arguments *parse_arguments(int argc, char *argv[])
         }
     }
 
-    return new Arguments(interface, tcp, udp, arp, icmp, number, port);
+    return std::make_unique<Arguments>(interface, tcp, udp, arp, icmp, number, port);
 }
--- projekt2/ipk-sniffer.h
+++ projekt2/ipk-sniffer.h
@@ -2,9 +2,33 @@
 #define IPK_SNIFFER_H
 
 #include "sniffer_classes.h"
+#include <memory>
 
+/**
+ * @brief Stuff to happen with the captured packet
+ * 
+ * @param user No idea what is this
+ * @param h Pcap packet header
+ * @param bytes Contents of the packet
+ */
+void packet_handler(u_char *user, const struct pcap_pkthdr *h, const u_char *bytes);
 
-Arguments *parse_arguments(int argc, char *argv[]);
+/**
+ * @brief Check if header found is extension header, that has another one behind.
+ * 
+ * @param header Current header
+ * @return true If the header was recognized and you should expect next one
+ * @return false Header was not recognized, probably is one of the upper layers header
+ */
+bool is_other_ext(int header);
+
+/**
+ * @brief Prints all avaliable network devices to stdin
+ * 
+ */
+void print_devices();
+
+std::unique_ptr<Arguments> parse_arguments(int argc, char *argv[]);
 
 
 #endif // IPK_SNIFFER_H
